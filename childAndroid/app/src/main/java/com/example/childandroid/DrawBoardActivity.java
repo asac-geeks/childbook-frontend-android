edd781d448211c751cd7aed8b7ee5ad2d2277254
areejobaid94 <areej.obaid@yahoo.com>
2021-06-10 11:02:36 +0300

    done-board

--- childAndroid/app/src/main/java/com/example/childandroid/DrawBoardActivity.java
+++ childAndroid/app/src/main/java/com/example/childandroid/DrawBoardActivity.java
@@ -7,6 +7,15 @@ import android.graphics.Paint;
 import android.graphics.Path;
 import android.view.View;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.github.nkzawa.emitter.Emitter;
+import com.github.nkzawa.socketio.client.IO;
+import com.github.nkzawa.socketio.client.Socket;
+
+import org.json.JSONObject;
+
+import java.net.URISyntaxException;
 
 import static com.example.childandroid.display.colors;
 import static com.example.childandroid.display.current_color;
@@ -15,11 +24,36 @@ import static com.example.childandroid.display.paths;
 public class DrawBoardActivity extends AppCompatActivity {
     static Path path = new Path();
     static Paint paint = new Paint();
-
+    private Socket mSocket = null;
+    private static final String URL = "http://localhost:9112/bord";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_draw_board);
+        try {
+            IO.Options ops = new IO.Options();
+            ops.forceNew = true;
+            ops.reconnection = true;
+            mSocket = IO.socket(URL, ops);
+
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+        mSocket.connect();
+
+        if (mSocket.connected()){
+            Toast.makeText(DrawBoardActivity.this, "Socket Connected!!",Toast.LENGTH_SHORT).show();
+            System.out.println("Socket Connected!!");
+            mSocket.on("/topic/messages/" + "areej", new Emitter.Listener() {
+            @Override
+            public void call(Object... args) {
+                JSONObject data = (JSONObject)args[0];
+                System.out.println(data);
+            }
+        });
+        }else{
+            System.out.println("not connect");
+        }
     }
     public void redColor(View view){
             paint.setColor(Color.RED);
--- childAndroid/app/src/main/java/com/example/childandroid/MainActivity.java
+++ childAndroid/app/src/main/java/com/example/childandroid/MainActivity.java
@@ -6,13 +6,27 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.github.nkzawa.emitter.Emitter;
+import com.github.nkzawa.socketio.client.IO;
+import com.github.nkzawa.socketio.client.Socket;
+
+import org.json.JSONObject;
+
+import java.net.URISyntaxException;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        SocketInstance instance = (SocketInstance)getApplication();
+//        ops.query = "Bearer " + "authToken";
+//        mSocket = instance.getSocketInstance();
+
     }
 
     public void goTo(View view) {
