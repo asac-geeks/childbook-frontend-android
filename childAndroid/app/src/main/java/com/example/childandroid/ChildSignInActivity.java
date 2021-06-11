100fbde8a68b188bb8d633a8c89e8c43675b76df
YazanSneneh <yaz.sneneh@gmail.com>
2021-06-11 20:51:51 +0300

    add login stuff

---
+++ childAndroid/app/src/main/java/com/example/childandroid/ChildSignInActivity.java
@@ -0,0 +1,14 @@
+package com.example.childandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChildSignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_child_sign_in);
+    }
+}
