34bdf3e19109e21be4d16d834d25c7f2fca91d60
SalahAlawneh <salah.alawneh@yahoo.com>
2021-06-16 15:21:49 +0300

    add post detail accept page UI

--- childAndroid/app/src/main/java/com/example/childandroid/TempPostDetailsActivity.java
+++ childAndroid/app/src/main/java/com/example/childandroid/TempPostDetailsActivity.java
@@ -1,16 +1,23 @@
 package com.example.childandroid;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
 import com.example.childandroid.modules.Post;
+import com.google.android.material.navigation.NavigationView;
 import com.google.gson.Gson;
 
 import org.jetbrains.annotations.NotNull;
@@ -26,14 +33,19 @@ import okhttp3.RequestBody;
 import okhttp3.Response;
 import okhttp3.logging.HttpLoggingInterceptor;
 
-public class TempPostDetailsActivity extends AppCompatActivity {
+public class TempPostDetailsActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     Post post = new Post();
     Context that = this;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_temp_post_details);
-        String url = "http://10.0.2.2:4040/post/public/"+  getIntent().getExtras().getString("id");
+        String url = "http://10.0.2.2:4040/post/public/" + getIntent().getExtras().getString("id");
         OkHttpClient httpClient = new OkHttpClient();
         Request request = new Request.Builder()
                 .url(url)
@@ -46,9 +58,10 @@ public class TempPostDetailsActivity extends AppCompatActivity {
             public void onFailure(@NotNull Call call, @NotNull IOException e) {
                 e.printStackTrace();
             }
+
             @Override
             public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
-                if(response.isSuccessful()){
+                if (response.isSuccessful()) {
                     Gson gson = new Gson();
                     // serialize
                     String body = response.body().string();
@@ -56,11 +69,11 @@ public class TempPostDetailsActivity extends AppCompatActivity {
                     System.out.println(body);
                     System.out.println(response.body());
                     System.out.println(response.body());
-                    post = gson.fromJson(body,  Post.class);
+                    post = gson.fromJson(body, Post.class);
                 }
             }
         });
-        try{
+        try {
             Thread.sleep(5000);
             TextView username = findViewById(R.id.temp_post_user);
             username.setText(post.getAppUser().getUserName());
@@ -68,23 +81,106 @@ public class TempPostDetailsActivity extends AppCompatActivity {
             body.setText(post.getBody());
             TextView title = findViewById(R.id.temp_post_title);
             title.setText(post.getPostTitle());
-        }catch(InterruptedException e){
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
+//        ======================navigation bar======================
 
+
+//        =======================================Hooks
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        toolbar = findViewById(R.id.toolbar_view);
+//        =======================================Tool Bar
+        setSupportActionBar(toolbar);
+//        =======================================Navigation Drawer Menu
+
+// ============================== Hide not needed items from navBar
+        Menu menu = navigationView.getMenu();
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        String checker = preferences.getString("token", "");
+        if (checker.equals("")) {
+            menu.findItem(R.id.nav_child_logout).setVisible(false);
+            menu.findItem(R.id.nav_parent_logout).setVisible(false);
+            menu.findItem(R.id.nav_child_profile).setVisible(false);
+            menu.findItem(R.id.nav_parent_profile).setVisible(false);
+            menu.findItem(R.id.nav_parent_login).setVisible(true);
+            menu.findItem(R.id.nav_child_login).setVisible(true);
+        } else {
+            menu.findItem(R.id.nav_child_logout).setVisible(true);
+            menu.findItem(R.id.nav_parent_logout).setVisible(true);
+            menu.findItem(R.id.nav_child_profile).setVisible(true);
+            menu.findItem(R.id.nav_parent_profile).setVisible(true);
+            menu.findItem(R.id.nav_parent_login).setVisible(false);
+            menu.findItem(R.id.nav_child_login).setVisible(false);
+        }
     }
 
-    public void postAccept(View view){
-        HttpLoggingInterceptor loggingInterceptor=new HttpLoggingInterceptor();
+    //    ==================================to prevent go out of the the app
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen((GravityCompat.START))) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull @NotNull MenuItem item) {
+        Intent intent = new Intent();
+        switch (item.getItemId()) {
+            case R.id.nav_home:
+                break;
+            case R.id.nav_youtube:
+                intent = new Intent(TempPostDetailsActivity.this, feedsActivity.class);
+                break;
+            case R.id.nav_our_games:
+                intent = new Intent(TempPostDetailsActivity.this, GamesPageActivity.class);
+                break;
+            case R.id.nav_whiteboard:
+                intent = new Intent(TempPostDetailsActivity.this, DrawBoardActivity.class);
+                break;
+            case R.id.nav_child_login:
+                intent = new Intent(TempPostDetailsActivity.this, ChildSignInActivity.class);
+                break;
+            case R.id.nav_child_profile:
+                intent = new Intent(TempPostDetailsActivity.this, ChildActivity.class);
+                break;
+            case R.id.nav_child_logout:
+                SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.remove("token");
+                editor.commit();
+                break;
+            case R.id.nav_parent_login:
+                intent = new Intent(TempPostDetailsActivity.this, ParentSignInActivity.class);
+                break;
+            case R.id.nav_parent_profile:
+                intent = new Intent(TempPostDetailsActivity.this, ParentActivity.class);
+                break;
+            case R.id.nav_parent_logout:
+                preferences = PreferenceManager.getDefaultSharedPreferences(this);
+                editor = preferences.edit();
+                editor.remove("token");
+                editor.commit();
+                break;
+        }
+        startActivity(intent);
+        return true;
+    }
+
+    public void postAccept(View view) {
+        HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
         loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
 
-        OkHttpClient client=new OkHttpClient.Builder()
+        OkHttpClient client = new OkHttpClient.Builder()
                 .addInterceptor(loggingInterceptor)
                 .build();
         String json = "{}";
         RequestBody requestBody = RequestBody.create(json, MediaType.parse("application/json"));
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
-        String token = "Bearer "+preferences.getString("token","");
+        String token = "Bearer " + preferences.getString("token", "");
         Request request = new Request.Builder()
                 .url("http://10.0.2.2:4040/postverification/" + getIntent().getExtras().getString("id"))
                 .header("Authorization", token)
@@ -98,10 +194,11 @@ public class TempPostDetailsActivity extends AppCompatActivity {
 //                e.printStackTrace();
 
             }
+
             @Override
             public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
-                if(response.isSuccessful()){
-                    String myResponse=response.body().string();
+                if (response.isSuccessful()) {
+                    String myResponse = response.body().string();
                     response.code();
                     response.isSuccessful();
                     Intent parentPageActivityIntent = new Intent(that, ChildSignInActivity.class);
