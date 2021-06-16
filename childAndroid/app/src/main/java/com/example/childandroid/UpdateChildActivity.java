412ad4103dd037bdcae6ec8fa3c759e7df00b0a2
SalahAlawneh <salah.alawneh@yahoo.com>
2021-06-16 10:55:10 +0300

    child update UI done

--- childAndroid/app/src/main/java/com/example/childandroid/UpdateChildActivity.java
+++ childAndroid/app/src/main/java/com/example/childandroid/UpdateChildActivity.java
@@ -1,6 +1,10 @@
 package com.example.childandroid;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.app.DatePickerDialog;
 import android.content.Context;
@@ -10,10 +14,14 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.google.android.material.navigation.NavigationView;
 
 import org.jetbrains.annotations.NotNull;
 
@@ -29,38 +37,126 @@ import okhttp3.RequestBody;
 import okhttp3.Response;
 import okhttp3.logging.HttpLoggingInterceptor;
 
-public class UpdateChildActivity extends AppCompatActivity {
-    String Birthdate="";
+public class UpdateChildActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    String Birthdate = "";
     private DatePickerDialog.OnDateSetListener mDataSetListner;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_chlil);
+
+        //        ======================navigation bar======================
+
+
+//        =======================================Hooks
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        toolbar = findViewById(R.id.toolbar_view);
+//        =======================================Tool Bar
+        setSupportActionBar(toolbar);
+//        =======================================Navigation Drawer Menu
+
+// ============================== Hide not needed items from navBar
+        Menu menu = navigationView.getMenu();
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        String checker = preferences.getString("token", "");
+        if (checker.equals("")) {
+            menu.findItem(R.id.nav_child_logout).setVisible(false);
+            menu.findItem(R.id.nav_parent_logout).setVisible(false);
+            menu.findItem(R.id.nav_child_profile).setVisible(false);
+            menu.findItem(R.id.nav_parent_profile).setVisible(false);
+            menu.findItem(R.id.nav_parent_login).setVisible(true);
+            menu.findItem(R.id.nav_child_login).setVisible(true);
+        } else {
+            menu.findItem(R.id.nav_child_logout).setVisible(true);
+            menu.findItem(R.id.nav_parent_logout).setVisible(true);
+            menu.findItem(R.id.nav_child_profile).setVisible(true);
+            menu.findItem(R.id.nav_parent_profile).setVisible(true);
+            menu.findItem(R.id.nav_parent_login).setVisible(false);
+            menu.findItem(R.id.nav_child_login).setVisible(false);
+        }
     }
 
-    public void updateChildButton(View view){
+    //    ==================================to prevent go out of the the app
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen((GravityCompat.START))) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull @NotNull MenuItem item) {
+        Intent intent = new Intent();
+        switch (item.getItemId()) {
+            case R.id.nav_home:
+                break;
+            case R.id.nav_youtube:
+                intent = new Intent(UpdateChildActivity.this, feedsActivity.class);
+                break;
+            case R.id.nav_our_games:
+                intent = new Intent(UpdateChildActivity.this, GamesPageActivity.class);
+                break;
+            case R.id.nav_whiteboard:
+                intent = new Intent(UpdateChildActivity.this, DrawBoardActivity.class);
+                break;
+            case R.id.nav_child_login:
+                intent = new Intent(UpdateChildActivity.this, ChildSignInActivity.class);
+                break;
+            case R.id.nav_child_profile:
+                intent = new Intent(UpdateChildActivity.this, ChildActivity.class);
+                break;
+            case R.id.nav_child_logout:
+                SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.remove("token");
+                editor.commit();
+                break;
+            case R.id.nav_parent_login:
+                intent = new Intent(UpdateChildActivity.this, ParentSignInActivity.class);
+                break;
+            case R.id.nav_parent_profile:
+                intent = new Intent(UpdateChildActivity.this, ParentActivity.class);
+                break;
+            case R.id.nav_parent_logout:
+                preferences = PreferenceManager.getDefaultSharedPreferences(this);
+                editor = preferences.edit();
+                editor.remove("token");
+                editor.commit();
+                break;
+        }
+        startActivity(intent);
+        return true;
+    }
+
+    public void updateChildButton(View view) {
         Context context = this;
-        EditText userName=findViewById(R.id.update_userName_child);
-        String username=userName.getText().toString();
+        EditText userName = findViewById(R.id.update_userName_child);
+        String username = userName.getText().toString();
 
-        EditText passWord=findViewById(R.id.update_password_child);
-        String password=passWord.getText().toString();
+        EditText passWord = findViewById(R.id.update_password_child);
+        String password = passWord.getText().toString();
 
-        EditText parentEmail=findViewById(R.id.update_email_child);
-        String parentemail=parentEmail.getText().toString();
+        EditText parentEmail = findViewById(R.id.update_email_child);
+        String parentemail = parentEmail.getText().toString();
 
 
-        HttpLoggingInterceptor loggingInterceptor=new HttpLoggingInterceptor();
+        HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
         loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
 
-        OkHttpClient client=new OkHttpClient.Builder()
+        OkHttpClient client = new OkHttpClient.Builder()
                 .addInterceptor(loggingInterceptor)
                 .build();
-        String json = "{\"userName\":\""+username+"\",\"password\":\""+password+"\",\"parentEmail\":\""+parentemail+"\"}";
+        String json = "{\"userName\":\"" + username + "\",\"password\":\"" + password + "\",\"parentEmail\":\"" + parentemail + "\"}";
         RequestBody requestBody = RequestBody.create(json, MediaType.parse("application/json"));
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
-        String token = "Bearer "+preferences.getString("token","");
+        String token = "Bearer " + preferences.getString("token", "");
         Request request = new Request.Builder()
                 .url("http://10.0.2.2:4040/profile")
                 .header("Authorization", token)
@@ -77,8 +173,8 @@ public class UpdateChildActivity extends AppCompatActivity {
 
             @Override
             public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
-                if(response.isSuccessful()){
-                    String myResponse=response.body().string();
+                if (response.isSuccessful()) {
+                    String myResponse = response.body().string();
                     response.code();
                     response.isSuccessful();
                     Intent parentPageActivityIntent = new Intent(context, ChildSignInActivity.class);
