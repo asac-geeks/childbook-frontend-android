3a38632b2b6a0599a3139ff5b80294080a910a74
SalahAlawneh <salah.alawneh@yahoo.com>
2021-06-16 12:50:08 +0300

    add post done the button is not in the right place

--- childAndroid/app/src/main/java/com/example/childandroid/AddPost_Activity.java
+++ childAndroid/app/src/main/java/com/example/childandroid/AddPost_Activity.java
@@ -3,6 +3,9 @@ package com.example.childandroid;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.ContentResolver;
 import android.content.Intent;
@@ -13,6 +16,8 @@ import android.os.storage.StorageManager;
 import android.preference.PreferenceManager;
 import android.text.TextUtils;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.webkit.MimeTypeMap;
 import android.widget.Button;
@@ -29,6 +34,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -52,7 +58,10 @@ import okhttp3.RequestBody;
 import okhttp3.Response;
 import okhttp3.logging.HttpLoggingInterceptor;
 
-public class AddPost_Activity extends AppCompatActivity {
+public class AddPost_Activity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
     private static final int PICK_VIDEO = 1;
     private VideoView videoView;
     private Button button;
@@ -64,6 +73,7 @@ public class AddPost_Activity extends AppCompatActivity {
     StorageReference storageReference;
     DatabaseReference databaseReference;
     UploadTask uploadTask;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -84,33 +94,118 @@ public class AddPost_Activity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent= new Intent();
+                Intent intent = new Intent();
                 intent.setType("video/*");
                 intent.setAction(Intent.ACTION_GET_CONTENT);
-                startActivityForResult(intent,PICK_VIDEO);
+                startActivityForResult(intent, PICK_VIDEO);
             }
         });
+
+        //        ======================navigation bar======================
+
+
+//        =======================================Hooks
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        toolbar = findViewById(R.id.toolbar_view);
+//        =======================================Tool Bar
+        setSupportActionBar(toolbar);
+//        =======================================Navigation Drawer Menu
+
+// ============================== Hide not needed items from navBar
+        Menu menu = navigationView.getMenu();
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        String checker = preferences.getString("token", "");
+        if (checker.equals("")) {
+            menu.findItem(R.id.nav_child_logout).setVisible(false);
+            menu.findItem(R.id.nav_parent_logout).setVisible(false);
+            menu.findItem(R.id.nav_child_profile).setVisible(false);
+            menu.findItem(R.id.nav_parent_profile).setVisible(false);
+            menu.findItem(R.id.nav_parent_login).setVisible(true);
+            menu.findItem(R.id.nav_child_login).setVisible(true);
+        } else {
+            menu.findItem(R.id.nav_child_logout).setVisible(true);
+            menu.findItem(R.id.nav_parent_logout).setVisible(true);
+            menu.findItem(R.id.nav_child_profile).setVisible(true);
+            menu.findItem(R.id.nav_parent_profile).setVisible(true);
+            menu.findItem(R.id.nav_parent_login).setVisible(false);
+            menu.findItem(R.id.nav_child_login).setVisible(false);
+        }
+    }
+
+    //    ==================================to prevent go out of the the app
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen((GravityCompat.START))) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull @NotNull MenuItem item) {
+        Intent intent = new Intent();
+        switch (item.getItemId()) {
+            case R.id.nav_home:
+                break;
+            case R.id.nav_youtube:
+                intent = new Intent(AddPost_Activity.this, feedsActivity.class);
+                break;
+            case R.id.nav_our_games:
+                intent = new Intent(AddPost_Activity.this, GamesPageActivity.class);
+                break;
+            case R.id.nav_whiteboard:
+                intent = new Intent(AddPost_Activity.this, DrawBoardActivity.class);
+                break;
+            case R.id.nav_child_login:
+                intent = new Intent(AddPost_Activity.this, ChildSignInActivity.class);
+                break;
+            case R.id.nav_child_profile:
+                intent = new Intent(AddPost_Activity.this, ChildActivity.class);
+                break;
+            case R.id.nav_child_logout:
+                SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.remove("token");
+                editor.commit();
+                break;
+            case R.id.nav_parent_login:
+                intent = new Intent(AddPost_Activity.this, ParentSignInActivity.class);
+                break;
+            case R.id.nav_parent_profile:
+                intent = new Intent(AddPost_Activity.this, ParentActivity.class);
+                break;
+            case R.id.nav_parent_logout:
+                preferences = PreferenceManager.getDefaultSharedPreferences(this);
+                editor = preferences.edit();
+                editor.remove("token");
+                editor.commit();
+                break;
+        }
+        startActivity(intent);
+        return true;
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == PICK_VIDEO){
+        if (requestCode == PICK_VIDEO) {
             videoUri = data.getData();
             videoView.setVideoURI(videoUri);
             uploadVideo();
         }
     }
 
-    private String getExt(Uri uri){
+    private String getExt(Uri uri) {
         ContentResolver contentResolver = getContentResolver();
         MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
         return mimeTypeMap.getExtensionFromMimeType(contentResolver.getType(uri));
     }
 
-    private void uploadVideo(){
+    private void uploadVideo() {
         String videoName = editText.getText().toString();
-        if(videoUri != null || !TextUtils.isEmpty(videoName)){
+        if (videoUri != null || !TextUtils.isEmpty(videoName)) {
             final StorageReference reference = storageReference.child(System.currentTimeMillis() + "." + getExt(videoUri));
             uploadTask = reference.putFile(videoUri);
             System.out.println(videoUri);
@@ -120,49 +215,49 @@ public class AddPost_Activity extends AppCompatActivity {
                 @Override
                 public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
                     System.out.println(reference.getDownloadUrl());
-                    if(task.isSuccessful()){
+                    if (task.isSuccessful()) {
                         throw task.getException();
                     }
                     return reference.getDownloadUrl();
                 }
-            }).addOnCompleteListener(new OnCompleteListener<Uri>(){
+            }).addOnCompleteListener(new OnCompleteListener<Uri>() {
                 @Override
                 public void onComplete(@NonNull Task<Uri> task) {
                     System.out.println("task.isSuccessful()");
                     System.out.println(task.isSuccessful());
-                    if(task.isSuccessful()){
+                    if (task.isSuccessful()) {
                         Uri downloadUri = task.getResult();
                         progressBar.setVisibility(View.INVISIBLE);
-                        Toast.makeText(AddPost_Activity.this,"DataSave Save", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(AddPost_Activity.this, "DataSave Save", Toast.LENGTH_SHORT).show();
                         member.setName(videoName);
                         member.setUrl(downloadUri.toString());
                         String i = databaseReference.push().getKey();
                         databaseReference.child(i).setValue(member);
-                    }else {
-                        Toast.makeText(AddPost_Activity.this,"Failed", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(AddPost_Activity.this, "Failed", Toast.LENGTH_SHORT).show();
                     }
                 }
             });
-        }else {
-            Toast.makeText(AddPost_Activity.this,"All Fields Are Required", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(AddPost_Activity.this, "All Fields Are Required", Toast.LENGTH_SHORT).show();
 
         }
     }
 
-    public void submitPost(View view){
+    public void submitPost(View view) {
         EditText title = findViewById(R.id.add_post_title);
         EditText body = findViewById(R.id.post_body_add);
 
-        HttpLoggingInterceptor loggingInterceptor=new HttpLoggingInterceptor();
+        HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
         loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
 
-        OkHttpClient client=new OkHttpClient.Builder()
+        OkHttpClient client = new OkHttpClient.Builder()
                 .addInterceptor(loggingInterceptor)
                 .build();
-        String json = "{\"postTitle\":\""+title.getText().toString()+"\",\"body\":\""+body.getText().toString()+"\",\"imageUrl\":\""+""+"\"}";
+        String json = "{\"postTitle\":\"" + title.getText().toString() + "\",\"body\":\"" + body.getText().toString() + "\",\"imageUrl\":\"" + "" + "\"}";
         RequestBody requestBody = RequestBody.create(json, MediaType.parse("application/json"));
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
-        String token = "Bearer "+preferences.getString("token","");
+        String token = "Bearer " + preferences.getString("token", "");
         Request request = new Request.Builder()
                 .url("http://10.0.2.2:4040/addPost")
                 .header("Authorization", token)
@@ -179,8 +274,8 @@ public class AddPost_Activity extends AppCompatActivity {
 
             @Override
             public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
-                if(response.isSuccessful()){
-                    String myResponse=response.body().string();
+                if (response.isSuccessful()) {
+                    String myResponse = response.body().string();
                     response.code();
                     response.isSuccessful();
                     Intent parentPageActivityIntent = new Intent(AddPost_Activity.this, ChildTemporary.class);
